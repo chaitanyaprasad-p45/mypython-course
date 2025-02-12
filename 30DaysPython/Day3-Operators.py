d85d42214857ea06bb271000c5a24f914c17c9c9
Chaitanya.Prasad <chaitanya.prasad@lycadigital.com>
2025-02-12 12:00:00 +0530

    initial commit

--- 30DaysPython/Day3-Operators.py
+++ 30DaysPython/Day3-Operators.py
@@ -58,3 +58,89 @@ print("The Circumference of the Circle is:", circumference)
 #12.    Find the length of 'python' and 'dragon' and make a falsy comparison statement.
 print(len("python"))
 print(len("dragon"))
+
+#13.    Use and operator to check if 'on' is found in both 'python' and 'dragon'
+print("on" in "python" and "on" in "dragon") # use the in operator along with and to check if "on" is present in both "python" and "dragon"
+
+#14.    I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+sentence = "I hope this course is not full of jargon."
+print("jargon" in sentence)
+
+#15.    There is no 'on' in both dragon and python
+print("on" not in "dragon" and "on" not in "python")
+
+#16.    Find the length of the text python and convert the value to float and convert it to string
+text = "python"
+print(len(text))
+print(float(len(text)))
+print(str(len(text)))
+
+"""
+text = "python"
+
+# Finding the length
+length = len(text)
+print(length)  # Output: 6
+
+# Converting to float
+length_float = float(length)
+print(length_float)  # Output: 6.0
+
+# Converting to string
+length_str = str(length)
+print(length_str)  # Output: "6"
+"""
+
+#17.    Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+x = int(input("Enter the Value: "))
+if x % 2 == 0:
+    print("This is a EVEN Number")
+else:
+    print("This is a ODD Number")
+
+#18.    Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+x = 7
+y = 3
+results = x // y
+converted_value = int(2.7)          # Convert 2.7 to an integer
+print(results == converted_value)    # Check if they are equal
+
+#19.    Check if type of '10' is equal to type of 10
+print(type('10') == type(10))
+
+#20.    Check if int('9.8') is equal to 10
+print(type(float('9.8')) == type(10))
+
+#21.    Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+x = int(input("Enter Hours: "))
+y = int(input("Enter Rate per Hour: "))
+z = x * y
+print("Your weekly earning is", z)
+
+#22.    Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+max_years = 100
+#(75×365)+(25×366)/100=365.25 days per year
+sec_in_year = 365.25 * 24 * 60 * 60
+x = int(input("Enter the Years you have lived: "))
+results = x * sec_in_year
+print("You have lived for", int(results), "seconds")
+
+#23.     Write a Python script that displays the following table
+"""
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+"""
+print("Number1", "1", "Number2", "Number3")
+for i in range (1, 6):
+    print(i, 1, i**2, i**3)
+
+"""
+print(f"{'Number':<8} {'1':<8} {'Number²':<8} {'Number³':<8}")
+
+for i in range(1, 6):
+    print(f"{i:<8} {1:<8} {i**1:<8} {i**2:<8} {i**3:<8}")
+#   <8 means left-align text in a column 8 spaces wide for a structured look.
+"""
